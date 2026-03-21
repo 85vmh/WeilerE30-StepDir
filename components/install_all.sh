14463f8b30c637c5b02d1d71b54ccab4e7bc7b51
85vmh <vasimihalca@gmail.com>
2026-03-21 14:20:04 +0200

    added custom halcompile, fixed program_state chuck_cover state

--- components/install_all.sh
+++ components/install_all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Path to halcompile. Override with: ./install_all.sh /path/to/halcompile.sh
+HALCOMPILE="${1:-halcompile}"
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for comp in "$SCRIPT_DIR"/*.comp; do
     echo "Installing: $(basename "$comp")"
-    sudo halcompile --install "$comp"
+    sudo "$HALCOMPILE" --install "$comp"
 done
